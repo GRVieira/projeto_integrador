d13955a9d048caa0c79f0d413f99ca2741a410ce
Yerro Cândido Ferreira <yerroferreira12@gmail.com>
2018-08-15 15:44:58 -0300

    Motor de Busca Finalizado

--- connection.php
+++ connection.php
@@ -13,53 +13,50 @@
 	$username = "root";
 	$password = "";
 	$dbname = "teste";
-	$nome =  isset($_POST['nome'])? $_POST['nome']: null;
+	$nome =  isset($_GET['nome'])? $_GET['nome']: null;
 	
-	if($nome == ""){
+	if($nome != ""){
 		
-		echo "<br/>";
-		echo "<h3>Você deve digitar um nome a ser pesquisado.</h3>";
 		
+		// Estabelece conexao
+		$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+		// Verifica a conexao criada
+		if(!$conn) {
+
+			die("Falha ao conectar: " . mysqli_connect_error());
+
+		}
+
+		$sql = "SELECT * FROM usuarios where nome like '%$nome%'";
+		
+		$result = mysqli_query($conn ,$sql);
+		
+		if($result == ""){
+			
+			echo "<br>";
+			echo "<h3>Resultados de" . $nome . ":</h3>";
+			echo "<hr/>";
+			echo "<p id='resultStyle'><strong>Nenhum resultado.</strong></p>";
+			
+		}
+		
+		while ($array_leitura = mysqli_fetch_array($result)){
+			
+			echo "<br/>";
+			echo "<h3>Resultados de " . $nome . ":</h3>";
+			echo "<hr/>";
+			echo "<p id='resultStyle'><strong>ID:</strong> " . $array_leitura['id_user'] . "<br/>";
+			echo "<strong>Nome:</strong>". $array_leitura['nome']. "<br/>";
+			echo "<strong>Login:</strong>". $array_leitura['login']. "<br/>";
+			echo "<br/>";
+			echo "<br/>";
+			
+			
+		}
+
+		mysqli_close($conn);
 	}
-	
-	// Estabelece conexao
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-	// Verifica a conexao criada
-	if(!$conn) {
-
-		die("Falha ao conectar: " . mysqli_connect_error());
-
-	}
-
-	$sql = "SELECT * FROM usuarios where nome like '%$nome%'";
-	
-	$result = mysqli_query($conn ,$sql);
-	
-	if($result == ""){
-		
-		echo "<br>";
-		echo "<h3>Resultados de" . $nome . ":</h3>";
-		echo "<hr/>";
-		echo "<p id='resultStyle'><strong>Nenhum resultado.</strong></p>";
-		
-	}
-	
-	while ($array_leitura = mysqli_fetch_array($result)){
-		
-		echo "<br/>";
-		echo "<h3>Resultados de " . $nome . ":</h3>";
-		echo "<hr/>";
-		echo "<p id='resultStyle'><strong>ID:</strong> " . $array_leitura['id_user'] . "<br/>";
-		echo "<strong>Nome:</strong>". $array_leitura['nome']. "<br/>";
-		echo "<strong>Login:</strong>". $array_leitura['login']. "<br/>";
-		echo "<br/>";
-		echo "<br/>";
-		
-	}
-
-	mysqli_close($conn);
-	
 ?>
 
 </body>
--- motor.php
+++ motor.php
@@ -1,21 +1,57 @@
 <?php 
-	include_once("connection.php");
+	//include_once("connection.php");
 ?>
 
 <html>
 	<head>
+	
 		<link rel="stylesheet" type="text/css" href="estilo.css" />
+		
 		<title>
-		|| Motor de Busca
+			|| Motor de Busca
 		</title>
+		
+		<script>
+		
+			function buscar(){
+				
+				var ajax = new XMLHttpRequest();
+				var nome = document.getElementById("nome").value;
+					
+				ajax.onreadystatechange = function(){
+						
+					if(ajax.readyState == 4){
+						document.getElementById("vem").innerHTML = ajax.responseText;
+					}
+						
+				};
+					
+				ajax.open("GET", "connection.php?nome=" + nome, true);
+				ajax.send();
+				
+			}
+		
+		</script>
+		
 	</head>
-<body>
-	<br/>
-	<form action="motor.php" method="POST">
-		<h3>Sistema de Busca</h3>
-		<p id="resultStyle">Nome: <br/>
-		<input type="text" name="nome" size="30"/>
-		<input type="submit" name="botaoEnviar" value="Buscar"/> </p>
-	</form>
-</body>
+	
+	<body>
+	
+		<br/>
+		
+
+			<h3>Sistema de Busca</h3>
+			<p id="resultStyle">Nome: <br/>
+			<input type="text" name="nome" id="nome" onkeyup="buscar()" size="30"/>
+			</p>
+			
+			<div id="vem">
+			
+			
+			
+			</div>
+
+		
+	</body>
+	
 </html>
